4b5c9d8bda0abe037e29cdf27285d62fab366051
fnsl1026 <fnsl1026@naver.com>
2025-02-05 14:56:52 +0900

    db 오류 수정

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,7 +1,8 @@
 const db = require('../config/db');
 
+// ✅ 모든 학생 정보 조회
 exports.getAllStudents = (callback) => {
-    db.query('SELECT * FROM students', (err, results) => {
+    db.query('SELECT * FROM student', (err, results) => {  // 수정: 'students' → 'student'
         if (err) {
             return callback(err);
         }
@@ -9,8 +10,9 @@ exports.getAllStudents = (callback) => {
     });
 };
 
+// ✅ 특정 학생 정보 조회
 exports.getStudentById = (id, callback) => {
-    db.query('SELECT * FROM students WHERE id = ?', [id], (err, result) => {
+    db.query('SELECT * FROM student WHERE id = ?', [id], (err, result) => {  // 수정: 'students' → 'student'
         if (err) {
             return callback(err);
         }
